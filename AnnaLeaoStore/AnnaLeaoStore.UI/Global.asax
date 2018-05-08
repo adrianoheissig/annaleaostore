fa8dd45554bc92a13f6f1eddbd0d1967731a6ede
Adriano Heissig <adriano@rosset.com.br>
2018-05-08 07:09:50 -0300

    Inicio do Projeto AnnaLeaoStore , criado solução e projetos em camada

---
+++ AnnaLeaoStore/AnnaLeaoStore.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnnaLeaoStore.UI.MvcApplication" Language="C#" %>
---
+++ AnnaLeaoStore/AnnaLeaoStore.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnnaLeaoStore.UI.Startup))]
+namespace AnnaLeaoStore.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
