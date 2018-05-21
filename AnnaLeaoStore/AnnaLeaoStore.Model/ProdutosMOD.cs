5aaad1c0a11cf43a0b52713aa32a4c7f461f0522
Adriano Heissig <adriano@rosset.com.br>
2018-05-21 18:53:19 -0300

    Inicio da Modelagem do Produto

--- AnnaLeaoStore/AnnaLeaoStore.Model/ProdutosMOD.cs
+++ AnnaLeaoStore/AnnaLeaoStore.Model/ProdutosMOD.cs
@@ -9,26 +9,18 @@ namespace AnnaLeaoStore.Model
 {
     public class ProdutosMOD
     {
-        public Int32 ID { get; set; }
+        public int? ID { get; set; }
         public string Referid { get; set; }
         public string Descricao { get; set; }
+        public string DescricaoSituacao { get; set; }
         public string Cor { get; set; }
-        public Int32 IDGrade { get; set; }
-        public Int32 IDFornecedor { get; set; }
-        public DateTime DataUltimaCompra { get; set; }
-        public Int32 Situacao { get; set; }
+        public DateTime? DataUltimaCompra { get; set; }
+        public int? Situacao { get; set; }
         public string Observacao { get; set; }
-        public string Tam1 { get; set; }
-        public string Tam2 { get; set; }
-        public string Tam3 { get; set; }
-        public string Tam4 { get; set; }
-        public string Tam5 { get; set; }
-        public string Tam6 { get; set; }
-        public string Tam7 { get; set; }
-        public string Tam8 { get; set; }
-        public string Tam9 { get; set; }
-        public string Tam10 { get; set; }
         public string LinkProduto { get; set; }
+
+        public virtual PessoasMOD Pessoas { get; set; }
+        public virtual GradeProdutosMOD Grade { get; set; }
 
 
 
---
+++ AnnaLeaoStore/AnnaLeaoStore.Repository/ProdutosREP.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using AnnaLeaoStore.DataAccess;
+using AnnaLeaoStore.Model;
+using AnnaLeaoStore.Repository.Extensions;
+
+namespace AnnaLeaoStore.Repository
+{
+    public class ProdutosREP
+    {
+        private ADO _ado = new ADO();
+
+        private string _strSQL;
+
+        public List<ProdutosMOD> GetID()
+        {
+            try
+            {
+                _strSQL = "LISTARPRODUTOSBASICO";
+
+                List<ProdutosMOD> produtos = new List<ProdutosMOD>();
+
+                DataTable registros = _ado.RetornarTabela(_strSQL, CommandType.StoredProcedure);
+
+                foreach (DataRow item in registros.Rows)
+                {
+                    produtos.Add(new ProdutosMOD
+                    {
+                        ID = item.GetValue<int>("ID"),
+                        Descricao = item.GetText("DESCRICAO"),
+                    });
+                }
+                return produtos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+        public void Deletar(int id)
+        {
+            try
+            {
+                _strSQL = "DELETARPRODUTO";
+                var cmd = new SqlCommand(_strSQL);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ID", id);
+
+                _ado.ExecutarSql(_ado.ObterCommand(cmd));
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Inserir(ContatosMOD contatos)
+        {
+            try
+            {
+                string storedProcedure = "INSERIRCONTATO";
+                var cmd = new SqlCommand(storedProcedure);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@IDTIPOREDESOCIAL", contatos.IDTipoRedeSocial);
+                cmd.Parameters.AddWithValue("@DESCRICAO", contatos.Descricao);
+                cmd.Parameters.AddWithValue("@IDPESSOAS", contatos.IDPessoa);
+
+                _ado.ExecutarSql(_ado.ObterCommand(cmd));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Atualizar(ContatosMOD contatos)
+        {
+            try
+            {
+                string storedProcedure = "ATUALIZARCONTATO";
+                var cmd = new SqlCommand(storedProcedure);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ID", contatos.ID);
+                cmd.Parameters.AddWithValue("@DESCRICAO", contatos.Descricao);
+
+                _ado.ExecutarSql(_ado.ObterCommand(cmd));
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
